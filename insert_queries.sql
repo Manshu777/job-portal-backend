93a1baed1a890fe102087042227a8aca0a3de8eb
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-12-14 19:37:22 +0530

    add-code

--- insert_queries.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE candidates 
-ADD COLUMN immediate_joiner TINYINT(1) NOT NULL DEFAULT 0 COMMENT '1 = Yes, 0 = No' AFTER notice_period,
-ADD COLUMN open_to_opportunities TINYINT(1) NOT NULL DEFAULT 1 COMMENT '1 = Yes (open to jobs), 0 = No' AFTER immediate_joiner;
-
-ALTER TABLE candidate_educations MODIFY complete_month VARCHAR(50);
